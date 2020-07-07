10e118f71dcb5082b3e28ef2cd41fc7e9afa3a53
Vihaga Yohan <vihagayohan94@gmail.com>
2020-07-07 10:56:37 +0530

    3_Implemented header (header separate component was not able to get the image from src folder. Therefore, impletemented header inside of App component - bug will be fixed later)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
+// components
+import Header from "./components/ui/Header";
+import logo from "./img/logo.png";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="container">
+      {/* header */}
+      <header className="center">
+        <img src={logo} alt="logo" />
+      </header>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/ui/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../../img/logo.png";
+
+const Header = () => {
+  return (
+    <Header>
+      <img src={logo} alt="" />
+    </Header>
+  );
+};
+
+export default Header;
